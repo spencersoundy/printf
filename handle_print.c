07605bc26aef2515016ad59228acaf4030e305a3
Sussocodes <puregoldsusan73@gmail.com>
2023-08-22 19:07:03 +0100

    handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -14,7 +14,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int m, unknow_len = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,11 +22,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (m = 0; fmt_types[m].fmt != '\0'; m++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[m].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[m].fmt == '\0')
+	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
